69e3b0e557afb35a5b162739b88da8a152a52f5e
Tyler Pottenger <tyler.pottenger@gmail.com>
2019-02-12 23:19:43 -0500

    Minor style changes

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
 use std::env;
 
-fn parse_args(args: &mut env::Args) -> String {
-    args.nth(1).unwrap_or_else(|| String::from("lol no input")).to_uppercase()
-}
-
+#[inline]
 fn exaggerate(s: &str) -> String {
-    s.chars().map(|c| format!("{} ", c)).collect()
+    s.chars().fold(String::new(), |s, c| s + &format!("{} ", c))
 }
 
 fn main() {
-    let jerk = parse_args(&mut env::args());
-    let space: String = vec![' '; jerk.len() * 2 - 3].into_iter().collect();
+    let jerk = env::args().nth(1)
+        .unwrap_or_else(|| String::from("lol no input"))
+        .to_uppercase();
+
+    let space = " ".repeat(jerk.len() * 2 - 3);
 
     println!("{}", exaggerate(&jerk));
     jerk.chars()
         .zip(jerk.chars().rev())
+        .take(jerk.len() - 1)
         .skip(1)
-        .take(jerk.len() - 2)
         .for_each(|(i, j)| println!("{}{}{}", i, space, j));
     println!("{}", exaggerate(&jerk.chars().rev().collect::<String>()));
 }
